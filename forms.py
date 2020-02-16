9a2eefb6b078262f9a089b36e9d038832ce7495d
anuraglamsal <vizculus@gmail.com>
2020-02-16 07:03:45 +0545

    Wrote some code in forms and main and explained what's happening. Just basic stuff.

---
+++ forms.py
@@ -0,0 +1,46 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField #Used to import classes for the boxes
+                                #in forms to write the required info in.
+
+                                # 1.) StringField is for info that is written
+                                # as strings like Names and such.
+
+from wtforms.validators import DataRequired #importing classes required for
+                                            #validation. Validators are used
+                                            #to set conditions in our fields and
+                                            #check if they are followed.
+
+                                            # 1.) DataRequired enables compulsa-
+                                            # ry requirement of data i.e. you
+                                            # cannot leave the field empty.
+
+class SignUp(FlaskForm): #In wtforms, if we want to use the resources
+                         #that it provides, we need to create a class that
+                         #inherits from the FlaskForm class imported from
+                         #the flask_wtf package. That is fundamental to do.
+                         #Why? The developers of wtforms know.
+
+    FirstName = StringField('First Name', validators=[DataRequired()])
+                                          #As our signup form should contain
+                                          #a field to write First Name in
+                                          #, we create a string field for that.
+                                          #For that, we create an object of the
+                                          #class 'StringField' that we imported.
+                                          #Now, we need to pass the required
+                                          #values  for attributes in that class.
+
+                                          #'First Name' inside the brackets is
+                                          #the label of the String Field
+                                          #typically written to indicate what
+                                          #you should write in the string field.
+                                          #We can explicitely use that label
+                                          #in our html.
+
+                                          #Next, we use validators. We pass on
+                                          #a variable named 'validators'
+                                          #containing a list of validator classes
+                                          #required for validation. It is also
+                                          #an attribute of the StringField class.
+                                          #I have explained the use of all the
+                                          #validator classes used in the import
+                                          #section of the code.
---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+app = Flask(__name__) #__name__ returns the name of the module--here
+                      #it's "main"--when it is used in some other module.
+                      #In the module itself however, it returns "__main__".
+                      #You can use this property to prevent your code
+                      #from running in other modules i.e. indicating that
+                      #the particular code is the "main code" like the creating
+                      #the main function in C. In this particular scenario,
+                      #Flask uses the name of the module to figure out where
+                      #all the things like templates, static files, etc.
+                      #of the application are stored. It basically sets up
+                      #our app. How it does it is black to me.
+
+@app.route("/") #This creates a path. Here particularly, if there is
+                #nothing written after '/' in the URL, the webpage under
+                #this is returned. It may be '/home' or '/about' or anything
+                #that you want. Flask uses decorators for this. Again,
+                #black box.
+
+def signup(): #Now, everything that you need to do for that particular
+              #path of the web app is done under this function.
