c3de63fa58ed384781248c5d841102e827c12230
anuraglamsal <vizculus@gmail.com>
2020-02-22 05:06:32 +0545

    Changed the comment layout in main and forms

--- forms.py
+++ forms.py
@@ -1,49 +1,49 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 
-                                #Used to import classes that provide fields
-                                #to do stuff like text boxes, submit buttons,
-                                #check boxes, etc.
+                         #Used to import classes that provide fields
+                         #to do stuff like text boxes, submit buttons,
+                         #check boxes, etc.
 
-                                # 1. StringField is for info that is written
-                                # as strings like names and such.
+                         # 1. StringField is for info that is written
+                         # as strings like names and such.
 
-                                # 2. PasswordField is for fields to enter
-                                # passwords securely. When you enter something
-                                # in a password field, it shows up as bullet
-                                # points of sorts.
+                         # 2. PasswordField is for fields to enter
+                         # passwords securely. When you enter something
+                         # in a password field, it shows up as bullet
+                         # points of sorts.
 
-                                # 3. SubmitField is used to generate the submit
-                                # button.
+                         # 3. SubmitField is used to generate the submit
+                         # button.
 
-                                # 4. BooleanField is used to generate a
-                                # checkbox. If you check it, the value is
-                                # True. If it is unchecked, the value is False.
-                                # The default value is False i.e unchecked.
+                         # 4. BooleanField is used to generate a
+                         # checkbox. If you check it, the value is
+                         # True. If it is unchecked, the value is False.
+                         # The default value is False i.e unchecked.
 
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-                                        #Here, we are importing classes for
-                                        #validation. Validators are used
-                                        #to set conditions in our fields and
-                                        #check if they are followed.
+                         #Here, we are importing classes for
+                         #validation. Validators are used
+                         #to set conditions in our fields and
+                         #check if they are followed.
 
-                                        # 1. DataRequired enables compulsa-
-                                        # ry requirement of data i.e. you
-                                        # cannot leave the field empty.
+                         # 1. DataRequired enables compulsa-
+                         # ry requirement of data i.e. you
+                         # cannot leave the field empty.
 
-                                        # 2. Length enables you to set
-                                        # maximum and minimum length bounds
-                                        # for text entered in the field.
+                         # 2. Length enables you to set
+                         # maximum and minimum length bounds
+                         # for text entered in the field.
 
-                                        # 3. The Email validator checks for
-                                        # accuracy in terms of structure of
-                                        # the given email.
+                         # 3. The Email validator checks for
+                         # accuracy in terms of structure of
+                         # the given email.
 
-                                        # 4. The EqualTo validator checks if
-                                        # the value entered in the field is
-                                        # equal to the value entered in some
-                                        # other field.
+                         # 4. The EqualTo validator checks if
+                         # the value entered in the field is
+                         # equal to the value entered in some
+                         # other field.
 
 class SignUp(FlaskForm): #In wtforms, if we want to use the resources
                          #that it provides, we need to create a class that
@@ -54,45 +54,45 @@ class SignUp(FlaskForm): #In wtforms, if we want to use the resources
     FirstName = StringField('First Name', validators=[DataRequired(),
                                                         Length(min=1, max=50)])
 
-                                    #As our signup form should contain
-                                    #a field to write 'First Name' in
-                                    #, we create a string field for that
-                                    #i.e. we create an object of the
-                                    #class 'StringField' that we imported.
-                                    #Now, we need to pass the required
-                                    #values for attributes in that class.
+                         #As our signup form should contain
+                         #a field to write 'First Name' in
+                         #, we create a string field for that
+                         #i.e. we create an object of the
+                         #class 'StringField' that we imported.
+                         #Now, we need to pass the required
+                         #values for attributes in that class.
 
-                                    #'First Name' inside the brackets is
-                                    #the label of the string field
-                                    #typically written to indicate what
-                                    #you should write in the string field.
-                                    #We can explicitely use that label
-                                    #in our html.
+                         #'First Name' inside the brackets is
+                         #the label of the string field
+                         #typically written to indicate what
+                         #you should write in the string field.
+                         #We can explicitely use that label
+                         #in our html.
 
-                                    #Next, we use validators. We pass on
-                                    #a variable named 'validators'
-                                    #containing a list of validator classes
-                                    #required for validation. It is also
-                                    #an attribute of the StringField class.
-                                    #I have explained the use of all the
-                                    #validator classes I've used in the import
-                                    #section of the code.
+                         #Next, we use validators. We pass on
+                         #a variable named 'validators'
+                         #containing a list of validator classes
+                         #required for validation. It is also
+                         #an attribute of the StringField class.
+                         #I have explained the use of all the
+                         #validator classes I've used in the import
+                         #section of the code.
 
     MiddleName = StringField('Middle Name (if any)')
 
-                                        #As there might not be a
-                                        #middle name, we are not
-                                        #using any validator here.
+                         #As there might not be a
+                         #middle name, we are not
+                         #using any validator here.
 
     LastName = StringField('Last Name', validators=[DataRequired(),
                                                     Length(min=1, max=50)])
 
     email = StringField('Email', validators=[DataRequired(), Email()])
 
-                                        #Here, we've used a pre-built
-                                        #email validator class to check
-                                        #for the validity of
-                                        #the entered email.
+                         #Here, we've used a pre-built
+                         #email validator class to check
+                         #for the validity of
+                         #the entered email.
 
     Username = StringField('Username', validators=[DataRequired(),
                                                    Length(min=5, max=20)])
@@ -100,48 +100,48 @@ class SignUp(FlaskForm): #In wtforms, if we want to use the resources
     Password = PasswordField('Password', validators=[DataRequired(),
                                                      Length(min=9, max=20)])
 
-                                        #Here, we create an object
-                                        #of the PasswordField class
-                                        #that we have imported above.
-                                        #Now, the field is suited to
-                                        #enter passwords.
+                         #Here, we create an object
+                         #of the PasswordField class
+                         #that we have imported above.
+                         #Now, the field is suited to
+                         #enter passwords.
 
     ConfirmPassword = PasswordField('Password', validators=[DataRequired(),
                                                   Length(min=9, max=20),
                                                   EqualTo('Password')])
 
-                                        #Here, we've used the EqualTo
-                                        #validator class to check if
-                                        #the value entered in the
-                                        #'ConfirmPassword' password field
-                                        #is equal to the value entered
-                                        #in the 'Password' password field
-                                        #that we've made above.
+                         #Here, we've used the EqualTo
+                         #validator class to check if
+                         #the value entered in the
+                         #'ConfirmPassword' password field
+                         #is equal to the value entered
+                         #in the 'Password' password field
+                         #that we've made above.
 
     Submit = SubmitField('Sign Up')
 
-                                        #Here, we have used the SubmitField
-                                        #class to generate the 'Submit'
-                                        #button used to submit the entered
-                                        #values and make them ready for
-                                        #validation.
+                         #Here, we have used the SubmitField
+                         #class to generate the 'Submit'
+                         #button used to submit the entered
+                         #values and make them ready for
+                         #validation.
 
-class Login(FlaskForm): #Here, we are creating another class that inherits
-                        #from the FlaskForm class imported above for our
-                        #login form. A login form typically requires a Email
-                        #field, a password field and a submit field.
-                        #A 'Remember me' tickbox is also typically used.
+class Login(FlaskForm):  #Here, we are creating another class that inherits
+                         #from the FlaskForm class imported above for our
+                         #login form. A login form typically requires a Email
+                         #field, a password field and a submit field.
+                         #A 'Remember me' tickbox is also typically used.
 
     email = StringField('Email', validators=[DataRequired(), Email()])
     Password = PasswordField('Password', validators=[DataRequired(),
                                                      Length(min=9, max=20)])
     Remember = BooleanField('Remember me')
 
-                                        #Added a 'Remember me' checkbox.
-                                        #We can use this to automatically
-                                        #open our logged in page without
-                                        #actually logging in every time. For
-                                        #that, we need to write extra code
-                                        #which we'll do in main and html.
+                         #Added a 'Remember me' checkbox.
+                         #We can use this to automatically
+                         #open our logged in page without
+                         #actually logging in every time. For
+                         #that, we need to write extra code
+                         #which we'll do in main and html.
 
     Submit = SubmitField('Sign In')
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template
-app = Flask(__name__) #__name__ returns the name of the module--here
-                      #it's "main"--when it is used in some other module.
-                      #In the module itself however, it returns "__main__".
-                      #You can use this property to prevent your code
-                      #from running in other modules i.e. indicating that
-                      #the particular code is the "main code" like creating
-                      #the main function in C. In this particular scenario,
-                      #Flask uses the name of the module to figure out where
-                      #all the things like templates, static files, etc.
-                      #of the application are stored. It basically sets up
-                      #our app. How it does it is black box to me.
+app = Flask(__name__)
 
-@app.route("/") #This creates a path. Here particularly, if there is
+                #__name__ returns the name of the module--here
+                #it's "main"--when it is used in some other module.
+                #In the module itself however, it returns "__main__".
+                #You can use this property to prevent your code
+                #from running in other modules i.e. indicating that
+                #the particular code is the "main code" like creating
+                #the main function in C. In this particular scenario,
+                #Flask uses the name of the module to figure out where
+                #all the things like templates, static files, etc.
+                #of the application are stored. It basically sets up
+                #our app. How it does it is black box to me.
+
+@app.route("/")
+                #This creates a path. Here particularly, if there is
                 #nothing written after '/' in the URL, the function under
                 #this is returned. It may be '/home' or '/about' or anything
                 #that you want. Flask uses decorators for this. Again,
                 #black box.
 
-def signup(): #Now, everything that you need to do for that particular
-              #path of the web app is done under this function.
+def signup():   #Now, everything that you need to do for that particular
+                #path of the web app is done under this function.
